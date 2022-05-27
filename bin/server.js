678b2a66b7da8706856e0d8383cb1d6526de0117
huchavez <43591173+huchavez@users.noreply.github.com>
2022-05-26 20:58:12 -0400

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ const app = require('../app');
 const server = http.createServer(app);
 
 
-const port = parseInt(process.env.port) || 80;
+const port = parseInt(process.env.port) || 5000;
 
 console.log("server is listening at %s", port);
 app.set('port', port);
